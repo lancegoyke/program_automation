3219bb103cf64a3d55e7b4e447b19a088a362380
Lance Goyke <lance@lancegoyke.com>
2025-04-14 09:46:13 -0500

    program 106

--- autoprog.py
+++ autoprog.py
@@ -32,7 +32,7 @@ SCOPES = [
 ]
 
 # You'll probably want to update this
-PROGRAM_NAME = "105"
+PROGRAM_NAME = "106"
 
 # These can probably stay the same
 DATA_SPREADSHEET_ID = "1tu0jNOpXEqCeEN4UKvk_Av5DE46CPNCjXBjDYZ6jhHQ"
